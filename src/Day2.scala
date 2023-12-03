18600cb88a9aa49aa7b81b8776c05cd8e4ec76a3
Adam Taylor <tayloral@tcd.ie>
2023-12-03 08:03:04 +0000

    part 2 day 2

--- src/Day2.scala
+++ src/Day2.scala
@@ -33,7 +33,34 @@ object Day2 {
 
   def part2(input: String): Unit = {
     println("-----------Part 2---------------")
+    val lines = input.split("\n")
+    val games = lines.map { line =>
+      val rounds = line.split(":")(1).split(";").toList
+      val game = line.replace("Game ", "").split(":").head.toInt
+      val results = rounds.map { round =>
+        //      println(round)
 
+        val bits = round.split(",")
+        val reds = bits.map(getColourCount(_, "red")).max
+        val blues = bits.map(getColourCount(_, "blue")).max
+        val greens = bits.map(getColourCount(_, "green")).max
+        val doable = reds <= 12 && blues <= 14 && greens <= 13
+        val out = Result(game = game, redMax = reds, blueMax = blues, greenMax = greens, possible = doable)
+        //        println(out)
+        out
+      }
+      results
+    }.toList
+
+    val powers = games.map {
+      list =>
+        val r = Math.max(list.map(_.redMax).max, 1)
+        val g = Math.max(list.map(_.greenMax).max, 1)
+        val b = Math.max(list.map(_.blueMax).max, 1)
+        r*g*b
+    }
+    println(powers)
+    println(powers.sum)
 
     println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
   }
